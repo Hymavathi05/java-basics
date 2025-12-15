3a76e47555f04e2844a34f7b12b28c28fbe4d58f
Hymavathi Kotha <kothahymavathi05@gmail.com>
2025-12-15 10:45:42 +0530

    Add basic variable declaration and output printing in Java
    This program demonstrates the fundamentals of Java programming by declaring variables of different data types (int and double) and printing their values to the console. It introduces basic syntax, variable initialization, and string concatenation using System.out.println(). This program serves as the foundation for understanding how data is stored and displayed in Java.

---
+++ basics/Print_Variables.java
@@ -0,0 +1,11 @@
+package com.pack1;
+public class Print_Variables 
+{
+	public static void main(String[] args) {
+		int age=22;
+	    double height=1.65;
+        System.out.println("Age: "+age);
+        System.out.println("Height:"+height);
+	}
+}
+
