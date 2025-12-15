339989cee2aeccb0ec1212e993d349627512abdc
Hymavathi Kotha <kothahymavathi05@gmail.com>
2025-12-15 11:03:19 +0530

    Compute factorial of a number using iterative multiplication
    This program calculates the factorial of a given number using a loop and a multiplication accumulator. It emphasizes correct initialization of the accumulator to 1 and demonstrates repeated multiplication logic. This program builds a strong foundation for understanding mathematical computations and iterative algorithms.

---
+++ basics/FactorialNumber.java
@@ -0,0 +1,16 @@
+package com.pack1;
+import java.util.Scanner;
+public class FactorialNumber {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		int N=sc.nextInt();
+		int factorial=1;
+		for(int i=1;i<=N;i++)
+		{
+			factorial=factorial*i;
+		}
+		System.out.println(factorial);
+		sc.close();
+		}
+	}
