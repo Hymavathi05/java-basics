9e71dc44e656a3398442c4c8868a5602044e338b
Hymavathi Kotha <kothahymavathi05@gmail.com>
2025-12-17 13:19:14 +0530

    Implement largest-of-three numbers comparison using if-else
    Created a Java program to find the largest among three user-input numbers.
    The solution uses nested if–else conditions to compare values without using
    arrays or built-in functions.
    The program correctly handles cases where numbers may be equal and ensures
    clear output indicating the largest value.

---
+++ basics/CheckLargestOfThree.java
@@ -0,0 +1,31 @@
+package com.pack1;
+import java.util.Scanner;
+public class CheckLargestOfThree {
+public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("Enter 1st number");
+		int a=sc.nextInt();
+		System.out.println("Enter 2nd number");
+		int b=sc.nextInt();
+		System.out.println("Enter 3rd number");
+		int c=sc.nextInt();
+		if(a==b && b==c)
+		{
+			System.out.println("All three numbers are equal");
+		}
+		else if(a >= b && a >=c)
+		{
+			System.out.println(a + " is largest number");
+		}
+		else if(b>=a && b>=c)
+			{
+			System.out.println(b + "is largest number");
+		}
+		else
+		{
+			System.out.println(c + " is largest number");
+		}
+			sc.close();
+	}
+
+}
