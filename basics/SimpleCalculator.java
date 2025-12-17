ddd7e64fa4a04c6fb39a62657133114618dbb5c8
Hymavathi Kotha <kothahymavathi05@gmail.com>
2025-12-17 13:22:02 +0530

    Create simple calculator using switch-case
    Developed a simple calculator program in Java using switch-case statements.
    The program performs basic arithmetic operations:
    - Addition
    - Subtraction
    - Multiplication
    - Division
    It also includes input validation to prevent division by zero and displays
    an appropriate message for invalid operators.

---
+++ basics/SimpleCalculator.java
@@ -0,0 +1,32 @@
+package com.pack1;
+import java.util.Scanner;
+public class SimpleCalculator {
+public static void main(String[] args) {
+	Scanner sc=new Scanner(System.in);
+	int a=sc.nextInt();
+	int b=sc.nextInt();
+	char op=sc.next().charAt(0);
+	switch(op)
+	{
+	case '+':
+		System.out.println(a+b);
+		break;
+	case '-':
+		System.out.println(a-b);
+		break;
+	case '*':
+		System.out.println(a*b);
+		break;
+	case '/':
+		if(b!=0)
+			System.out.println("Division by zero not allowed");
+		else
+		System.out.println(a/b);
+		break;
+		default:System.out.println("Invalid operation");
+	}
+	sc.close();
+
+	}
+
+}
